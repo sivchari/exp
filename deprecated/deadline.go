8ce66bf7c4e948eb5ef1b8d7beefa35c4ca9f047
sivchari <shibuuuu5@gmail.com>
2024-02-12 15:32:39 +0900

    /tmp deperecated
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

---
+++ deprecated/deadline.go
@@ -0,0 +1,53 @@
+package deprecated
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var (
+	once sync.Once
+	t    time.Time
+)
+
+func Setoff(t time.Time) {
+	if t.Before(time.Now()) {
+		panic("time is in the past")
+	}
+	once.Do(func() {
+		t = t
+	})
+}
+
+func SetoffDuration(d time.Duration) {
+	Setoff(time.Now().Add(d))
+}
+
+type DeprecatedDeadlineRoundTrip struct{}
+
+var _ http.RoundTripper = (*DeprecatedDeadlineRoundTrip)(nil)
+
+func (d *DeprecatedDeadlineRoundTrip) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.IsZero() {
+		return nil, fmt.Errorf("deadline not set")
+	}
+	if time.Now().After(t) {
+		slog.Warn("deadline exceeded", slog.String("reason", fmt.Sprintf("the time setoff is %s", t.String())))
+	}
+	return http.DefaultTransport.RoundTrip(req)
+}
+
+func Middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if t.IsZero() {
+			http.Error(w, "deadline not set", http.StatusInternalServerError)
+		}
+		if time.Now().After(t) {
+			slog.Warn("deadline exceeded", slog.String("reason", fmt.Sprintf("the time setoff is %s", t.String())))
+		}
+		h.ServeHTTP(w, r)
+	})
+}
