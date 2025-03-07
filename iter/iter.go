d7844085adce29de685860b21d14d277449cd079
sivchari <shibuuuu5@gmail.com>
2025-03-07 17:45:03 +0900

    add
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- iter/iter.go
+++ iter/iter.go
@@ -15,3 +15,23 @@ func Chain[T any](iters ...iter.Seq[T]) iter.Seq[T] {
 		}
 	}
 }
+
+func KeyMap[K comparable](iter iter.Seq[K]) iter.Seq2[K, struct{}] {
+	return func(yield func(K, struct{}) bool) {
+		for k := range iter {
+			if !yield(k, struct{}{}) {
+				return
+			}
+		}
+	}
+}
+
+func ValueMap[V any](iter iter.Seq[V]) iter.Seq2[struct{}, V] {
+	return func(yield func(struct{}, V) bool) {
+		for v := range iter {
+			if !yield(struct{}{}, v) {
+				return
+			}
+		}
+	}
+}
